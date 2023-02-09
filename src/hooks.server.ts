7615942e9adfe320080f2289216baa8de9d78e29
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-09 15:28:32 +0000

    secrets sent

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { GITHUB_ID, MY_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: MY_SECRET })
+  ],
 })
